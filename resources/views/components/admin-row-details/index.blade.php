285f37cb4375e71e8690a01d01f1a9989752d765
isratahamedsabbir <93650049+isratahamedsabbir@users.noreply.github.com>
2026-09-11 12:31:50 +0600

    u

--- resources/views/components/admin-row-details/index.blade.php
+++ resources/views/components/admin-row-details/index.blade.php
@@ -2,7 +2,7 @@
 
 <tr class="bg-zinc-50/60 lg:hidden">
     <td colspan="{{ $colspan }}" class="px-4 py-1">
-        <div class="divide-y divide-zinc-100 max-w-xl">
+        <div class="divide-y divide-zinc-100">
             {{ $slot }}
         </div>
     </td>
